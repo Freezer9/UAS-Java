a024aa01ab4c809b4c6cf2a08efb9b6cf8acfe54
Freezer9 <farismuhammadabd@gmail.com>
2023-11-21 12:23:11 +0700

    Maven Java

---
+++ src/main/java/Class/Login.java
@@ -0,0 +1,8 @@
+package Class;
+
+public class Login {
+    public static void main(String[] args) {
+        View.SignIn LoginFrame = new View.SignIn();
+        LoginFrame.setVisible(true);
+    }
+}
